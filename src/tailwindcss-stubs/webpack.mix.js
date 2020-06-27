709d7ebd24ed60a44bd3b896b074f16d0d5f968c
leguy-design <62065918+leguy-design@users.noreply.github.com>
2020-06-27 14:57:33 +0200

    Update webpack.mix.js

--- src/tailwindcss-stubs/webpack.mix.js
+++ src/tailwindcss-stubs/webpack.mix.js
@@ -14,10 +14,9 @@ require('laravel-mix-tailwind');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-   .postCss('resources/css/app.css', 'public/css')
+   .sass('resources/scss/app.scss', 'public/css')
    .tailwind('./tailwind.config.js');
 
 if (mix.inProduction()) {
-  mix
-   .version();
+  mix.version();
 }
